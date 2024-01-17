9300047f7496fabcd2571a1ddf58f2e7a34fcf03
gogicha007 <gogicha@gmail.com>
2024-01-17 23:24:34 +0400

    feat: second task done

---
+++ 02-write-file/index.js
@@ -0,0 +1,28 @@
+const { stdin, stdout, stderr } = process;
+const fs = require('fs');
+const path = require('path');
+const file = path.join(__dirname, 'text.txt');
+
+fs.writeFile(file, '', (err) => {
+  if (err) console.log('Error occured when writing to file');
+});
+
+const writeToFile = (data) => {
+  fs.appendFile(file, data, (err) => {
+    if (err) {
+      console.log('Error occured when writing to file');
+    }
+  });
+};
+
+stdout.write('Please enter the data:\n');
+stdin.setEncoding('utf-8');
+stdin.on('data', (data) => {
+  const inData = data;
+  writeToFile(data);
+});
+
+process.on('SIGINT', () => {
+  stderr.write(`Good bye...`);
+  process.exit();
+});
