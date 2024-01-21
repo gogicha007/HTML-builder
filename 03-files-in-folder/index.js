bc94aaa09c7a1ac3dec4db122af79ab040b9ccb5
gogicha007 <gogicha@gmail.com>
2024-01-21 13:27:05 +0400

    refactor: third task

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -3,26 +3,24 @@ const path = require('path');
 
 const secretDir = path.join(__dirname, 'secret-folder');
 
-const fileStat = async (file) => {
+const printName = (file) => {
   const name = path.parse(file).name;
   const ext = path.parse(file).ext.substring(1);
   fs.stat(path.join(secretDir, file), (error, stats) => {
-    if (error) console.log(`Something went wrong with error code: ${error}`);
+    if (error) console.log(`Cannot get stats for file error code: ${error}`);
     else {
-      if (stats.isFile()) {
-        console.log(`${name} - ${ext} - ${stats.size / 1000}kb`);
-      }
+      console.log(`${name} - ${ext} - ${stats.size / 1000}kb`);
     }
   });
 };
 
 fs.promises
-  .readdir(secretDir)
-  .then(async (files) => {
+  .readdir(secretDir, { withFileTypes: true })
+  .then((files) => {
     for (const file of files) {
-      fileStat(file);
+      if (file.isFile()) printName(file.name);
     }
   })
   .catch((err) => {
-    console.log(`Something went wrong with error code: ${err}`);
+    console.log(`Something went wrong, error code: ${err}`);
   });
